33e2a631617cc7641acdff6c2bc3dc88f8516e4c
kelly <kelly.tate.dev@gmail.com>
2023-05-19 09:35:18 -0700

    Improves dark mode functionality. Allows mode preference across refresh and page navigation.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-class CssPropControl {
-  constructor(element) {
-    this.element = element;
-  }
-  get(varName) {
-    return getComputedStyle(this.element).getPropertyValue(varName);
-  }
-  set(varName, val) {
-    return this.element.style.setProperty(varName, val);
-  }
-}
-
-const bodyCssProps = new CssPropControl(document.body);
-
 // Adds navigation functionality.
 let contactBtn = document.querySelector('#contact-link');
 
@@ -101,26 +87,26 @@ indexBtn.addEventListener('click', () => {
 // Adds dark/light mode functionality
 let toggle = document.querySelector('#dark-mode-toggle');
 toggle.addEventListener('click', () => { 
-  setMode();
-  save();
+  updateMode();
 })
 
-function setMode() {
+function updateMode() {
   let mode = toggle.checked ? 'dark' : 'light';
-  bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`));
-  bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`));
-  bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`));
+  saveColorMode(mode);
+  setColorMode(mode);
+  saveToggle();
 }
 
-function load() {    
-  var checked = JSON.parse(localStorage.getItem('dark-mode-toggle'));
+function loadToggle() {    
+  var checked = JSON.parse(window.localStorage.getItem('dark-mode-toggle'));
   document.getElementById("dark-mode-toggle").checked = checked;
-  setMode();
 }
 
-function save() {
+loadToggle();
+
+function saveToggle() {
   var checkbox = document.getElementById('dark-mode-toggle');
-  localStorage.setItem('dark-mode-toggle', checkbox.checked);
+  window.localStorage.setItem('dark-mode-toggle', checkbox.checked);
 }
 
 
